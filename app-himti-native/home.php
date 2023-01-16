8588d5d1a66d125b9c51dc92a4af4f4390fd1d21
NurhijriaRahmadhani <nurhijria20@mhs.akba.ac.id>
2023-01-16 10:08:09 +0800

    App HIMTI Native

---
+++ app-himti-native/home.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6 text-center">
+            <p>&nbsp;</p>
+            <p>NIM : 20202205003</p>
+            <p>Nama Mahasiswa : Nurhijria Rahmadhani</p>
+        </div>
+    </div>
+</div>
